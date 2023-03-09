337f39071e43602231908ccc748d06f47ec9b333
Joshua Jensen <ender2821@yahoo.com>
2023-03-08 20:58:06 -0700

    finish building and styling header/nav

--- app/Header.jsx
+++ app/Header.jsx
@@ -1,68 +1,133 @@
-'use client';
-import { useState } from 'react';
-import styles from './Header.module.scss';
-import { useRouter, usePathname } from 'next/navigation';
+"use client";
+import { useState } from "react";
+import { useMediaQuery } from 'react-responsive'
+import styles from "./Header.module.scss";
 import Image from "next/image";
 import Link from "next/link";
 
-import MenuIcon from '../public/menu-icon.svg';
-import CloseIcon from '../public/close-icon.svg';
-import SearchIcon from '../public/search-icon.svg';
+import { Roboto_Slab } from "next/font/google";
+
+import MenuIcon from "../public/menu-icon.svg";
+import CloseIcon from "../public/close-icon.svg";
+import CloseIconDark from "../public/close-icon-dark.svg";
+import SearchIcon from "../public/search-icon.svg";
+
+import { navData } from '../fakeData';
+
+const robotoSlab = Roboto_Slab({ subsets: ["latin"] });
+
+
+
 
 export default function Header() {
-    const router = useRouter();
-    const path = usePathname();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const isMobile = useMediaQuery({ query: '(max-width: 767px)' })
 
-    const [ menuOpen, setMenuOpen ] = useState(false);
-  
-    return (
-      <header className={styles.header}>
-        {menuOpen ? (
-        <button className={styles.menuButton} onClick={() => setMenuOpen(false)}>
-          <CloseIcon />
-          <span>CLOSE</span>
-        </button>
-        ) : (
-          <button className={styles.menuButton} onClick={() => setMenuOpen(true)}>
-            <MenuIcon />
-            <span>MENU</span>
-
-          </button>
-        )}
-        <div className={styles.headerContent}>
-          <Image
-            src={"/RFALogoFinal.png"}
-            alt={"RFA Logo"}
-            height={100}
-            width={100}
-          />
-          <div classNAme={styles.headerTextContain}>
-            <p>South Carolina</p>
-            <p>Revinue and fiscal affairs office</p>
-          </div>
-          <nav>
-            <ul>
-              <li>
-                <Link href={"/"}>
-                  About us
-                </Link>
-              </li>
-              <li>
-                <Link href={"/"}>
-                  Events
-                </Link>
-              </li>
-              <li>
-                <Link href={"/"}>
-                  Boards &amp; Committees
-                </Link>
-              </li>
-            </ul>
-          </nav>
-          <button className={styles.searchButton}>
-            <SearchIcon />
-          </button>
-        </div>
-      </header>
+  const navSectionRender = (number) => {
+      return (
+      <ul>
+        {navData[number]?.map((item, i) => {
+          return (
+            <li key={i}>
+              <Link href={item?.link}>
+                {item?.name}
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
     )
   }
+
+  return (
+    <header className={styles.header}>
+      {!isMobile &&
+        (menuOpen ? (
+          <button
+            className={styles.menuButton}
+            onClick={() => setMenuOpen(false)}
+          >
+            <div>
+              <CloseIcon />
+              <span>CLOSE</span>
+            </div>
+          </button>
+        ) : (
+          <button
+            className={styles.menuButton}
+            onClick={() => setMenuOpen(true)}
+          >
+            <div>
+              <MenuIcon />
+              <span>MENU</span>
+            </div>
+          </button>
+        ))}
+      
+
+      <div className={styles.headerContent}>
+        {isMobile && (<button
+          className={styles.menuButton}
+          onClick={() => setMenuOpen(true)}
+          >
+          <div>
+            <MenuIcon />
+          </div>
+        </button>)}
+        <Image
+          src={"/RFALogoFinal.png"}
+          alt={"RFA Logo"}
+          height={isMobile ? 40 : 100}
+          width={isMobile ? 40 : 100}
+        />
+        <div className={styles.headerTextContain}>
+          <p className={robotoSlab.className}>South Carolina</p>
+          <p className={robotoSlab.className}>
+            Revinue and fiscal affairs office
+          </p>
+          {!isMobile && (<p className={styles.subhead}>
+            Transforming data into solutions for south carolina
+          </p>)}
+        </div>
+        {!isMobile && (
+        <nav>
+          <ul>
+            <li>
+              <Link href={"/"}>About us</Link>
+            </li>
+            <li>
+              <Link href={"/"}>Events</Link>
+            </li>
+            <li>
+              <Link href={"/"}>Boards &amp; Committees</Link>
+            </li>
+          </ul>
+        </nav>
+        )}
+        <button className={styles.searchButton}>
+          <SearchIcon />
+        </button>
+      </div>
+      {!isMobile && <span className={styles.divider}/>}
+      {menuOpen && (
+        <div className={styles.popup}>
+          {isMobile && (
+            <button
+              className={styles.mobileCloseIcon}
+              onClick={() => setMenuOpen(false)}
+            >
+              <CloseIconDark />
+            </button>
+          )}
+          <nav className={styles.navContain}>
+            {navSectionRender(0)}
+            {navSectionRender(1)}
+            {navSectionRender(2)}
+            {navSectionRender(3)}
+            {navSectionRender(4)}
+          </nav>
+        </div>
+      )}
+    </header>
+  );
+}
---
+++ fakeData.ts
@@ -0,0 +1,142 @@
+
+
+export const navData = [
+    [
+        {
+            name: 'Home',
+            link: '/'
+        },
+        {
+            name: 'News & Events',
+            link: '/'
+        },
+        {
+            name: 'About US',
+            link: '/'
+        },
+        {
+            name: 'Contact US',
+            link: '/'
+        },
+        {
+            name: 'Boards & Committees',
+            link: '/'
+        },
+    ],
+    [
+        {
+            name: 'Data & Research',
+            link: '/'
+        },
+        {
+            name: 'Economy',
+            link: '/'
+        },
+        {
+            name: 'State Finances',
+            link: '/'
+        },
+        {
+            name: 'Education',
+            link: '/'
+        },
+        {
+            name: 'Healthcare',
+            link: '/'
+        },
+        {
+            name: 'Local Government',
+            link: '/'
+        },
+        {
+            name: 'Population and Demographics',
+            link: '/'
+        },
+        {
+            name: 'State Registar Calculations',
+            link: '/'
+        },
+        {
+            name: 'Presentations',
+            link: '/'
+        },
+    ],
+    [
+        {
+            name: 'Geography & Mapping',
+            link: '/'
+        },
+        {
+            name: 'Transportation Carrier Network (Uber)',
+            link: '/'
+        },
+        {
+            name: 'SC Real Time Network',
+            link: '/'
+        },
+        {
+            name: 'SC State GIS',
+            link: '/'
+        },
+        {
+            name: 'Maps',
+            link: '/'
+        },
+        {
+            name: 'Interactive Mapping',
+            link: '/'
+        },
+        {
+            name: 'Statewide Aerial Imagery',
+            link: '/'
+        },
+    ],
+    [
+        {
+            name: 'Programs & Services',
+            link: '/'
+        },
+        {
+            name: 'Precint Demographics and Redistricting',
+            link: '/'
+        },
+        {
+            name: 'State 911 Program',
+            link: '/'
+        },
+        {
+            name: 'Data Services and Online Solutions',
+            link: '/'
+        },
+        {
+            name: 'Geodetic Survey',
+            link: '/'
+        },
+        {
+            name: 'Fiscal Analysis',
+            link: '/'
+        },
+        {
+            name: '2020 Census',
+            link: '/'
+        },
+    ],
+    [
+        {
+            name: 'Legislative Fiscal Impacts',
+            link: '/'
+        },
+        {
+            name: 'Current General Assembly Session',
+            link: '/'
+        },
+        {
+            name: 'Previous General Assembly Session',
+            link: '/'
+        },
+        {
+            name: 'Forms',
+            link: '/'
+        },
+    ]
+];
